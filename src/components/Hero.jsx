e93d74f765d45506a892ce11f0cfa798d258c0c0
Sameh <sameh.abdelhaleem95@gmail.com>
2024-03-07 11:12:12 +0200

    style Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import illsutrationIntro from "../assets/illustration-intro.png";
+const Hero = () => {
+  return (
+    <section
+      id="hero"
+      className="bg-curvy-light-mode dark:bg-curvy-dark-mode bg-no-repeat bg-contain bg-bottom "
+    >
+      {/* Hero Container */}
+      <div className="container mx-auto px-6 text-center md:pt-20 pb-52">
+        <img src={illsutrationIntro} alt="" className="mx-auto" />
+        <h1 className="max-w-2xl mx-auto mb-10 text-3xl font-bold leading-normal mt-14 md:text-4xl">
+          All your files in one secure location, assessible anywhere.
+        </h1>
+        <p className="max-w-sm mx-auto mb-10 text-sm md:max-w-xl md:text-lg">
+          Fylo stores all your most important files in one secure location.
+          Access them wherever you need, share and collaborate with friends
+          family, and co-workers.
+        </p>
+        <button className="p-3 rounded-full w-52 bg-accentCyan hover:scale-95">
+          Get Started
+        </button>
+      </div>
+    </section>
+  );
+};
+export default Hero;
